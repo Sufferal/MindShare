994a4bf8589429c3234b1a6f5f0d8d5c1805dcb6
Ciprian <48862508+Sufferal@users.noreply.github.com>
2023-09-24 13:43:46 +0300

    chore: create a new angular app (#1)

---
+++ UI/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch:'full',
+    redirectTo: 'login'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
