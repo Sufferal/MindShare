c969312dd02aa2e19c3b7792d9f8dc4016ee2308
Grena30 <grenadinu@yahoo.com>
2023-10-04 20:53:43 +0300

    feat: add basic GET request

--- Server/Controllers/ServerController.cs
+++ Server/Controllers/ServerController.cs
@@ -10,28 +10,10 @@ namespace MindShare.ServerApp.Controllers
     public class ServerController : ControllerBase
     {
         [HttpGet]
-        public string Get()
+        public IActionResult GetHello()
         {
-            // Your existing server logic goes here
-            string ipAddress = "127.0.0.1";
-            int port = 8080;
-            TcpClient client = new TcpClient(ipAddress, port);
-            NetworkStream stream = client.GetStream();
-
-            // Send a sample request to your server
-            string request = "Hello, Server!";
-            byte[] requestData = Encoding.ASCII.GetBytes(request);
-            stream.Write(requestData, 0, requestData.Length);
-
-            // Read the response from your server
-            byte[] buffer = new byte[1024];
-            int bytesRead = stream.Read(buffer, 0, buffer.Length);
-            string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-
-            // Close the client connection
-            client.Close();
-
-            return $"Response from server: {response}";
+            string message = "Hello, World!";
+            return Ok(message);
         }
     }
 }
