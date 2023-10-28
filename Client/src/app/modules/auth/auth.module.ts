a695a7867fde23d4e8beb3942e3047b9918c990e
Sufferal <botnari_ciprian@mail.ru>
2023-10-28 09:11:12 +0300

    refactor: update routing paths

--- Client/src/app/modules/auth/auth.module.ts
+++ Client/src/app/modules/auth/auth.module.ts
@@ -7,27 +7,13 @@ import { RegisterComponent, LoginComponent } from './components';
 import { Routes, RouterModule } from '@angular/router';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: AuthPageComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'register',
-    component: RegisterComponent,
-  },
-];
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [AuthPageComponent, RegisterComponent, LoginComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes),
+    AuthRoutingModule,
     ReactiveFormsModule,
     FormsModule,
     MatIconModule,
---
+++ Client/src/app/modules/auth/pages/index.ts
@@ -0,0 +1,1 @@
+export { AuthPageComponent } from './auth-page/auth-page.component'
