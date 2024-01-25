ecc5c193c0be8a18b29af766ab6ad87aa66a25e9
Dinu <grenadinu@yahoo.com>
2024-01-25 15:00:29 +0200

    fix: add user data to 2FA return message (#34)

--- Server/Controllers/AuthController.cs
+++ Server/Controllers/AuthController.cs
@@ -33,16 +33,9 @@ public class AuthController : ControllerBase
     {
         try
         {
-            var isValidToken = await _authService.TwoStepAuth(model.Username, model.Token);
-
-            if (isValidToken)
-            {
-                return Ok(new { status = 200, message = $"Two-step authentication successful for user {model.Username}", data = model });
-            }
-            else
-            {
-                return Ok(new { status = 401, message = "Invalid two-step authentication token." });
-            }
+            var user = await _authService.TwoStepAuth(model.Username, model.Token);
+            return Ok(new { status = 200, message = $"Two-step authentication successful for user {model.Username}", data = user });
+            
         }
         catch (Exception ex)
         {
